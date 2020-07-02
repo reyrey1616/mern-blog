1b536fe41b2bf659c9b8372f2bb17f44a090ba0c
yobbad16 <dabboyrey@gmail.com>
2020-07-03 04:22:18 +0800

    Created Global Styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Spinner from './components/global/Spinner';
 import Navbar from './components/Navbar/Navbar';
-
+import GlobalStyles from './styles/Global.styles';
 // Dynamic Imports
 const LoginPage = lazy(() => import('./pages/Login/Login.page'));
 const SignupPage = lazy(() => import('./pages/Signup/Signup.page'));
@@ -11,6 +11,7 @@ const HomePage = lazy(() => import('./pages/Home/Home.page'));
 const App = () => {
   return (
     <div>
+      <GlobalStyles />
       <Suspense fallback={<Spinner />}>
         <BrowserRouter>
           <Navbar />
---
+++ src/styles/Global.styles.jsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+
+      .flex {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+      }`;
+export default GlobalStyles;
