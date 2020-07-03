d07585802b9f3b1a5b2ba7b66d659fdb793c4e91
yobbad16 <dabboyrey@gmail.com>
2020-07-04 04:35:31 +0800

    Created controller for creating blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -17,3 +17,20 @@ exports.getBlogs = asyncHandler(async (req, res, next) => {
     return res.status(500).json({ error });
   }
 });
+
+// @desc CREATE BLOG
+// @route POST /api/blogs
+// @access Private
+
+exports.createBlogs = asyncHandler(async (req, res, next) => {
+  try {
+    const blog = await Blogs.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: blog,
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
