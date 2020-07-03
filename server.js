17e40127fb1bceeb59610ae960859318b0837ded
yobbad16 <dabboyrey@gmail.com>
2020-07-04 00:21:56 +0800

    Setup the node server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log(`Server is running on port ${PORT}`);
+});
