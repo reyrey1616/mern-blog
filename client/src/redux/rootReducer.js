9ceda240b694a61994ea11aecc0c68b1b6c37b80
yobbad16 <dabboyrey@gmail.com>
2020-07-06 05:16:03 +0800

    Added types for user reducer

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
 import alerts from './alerts/alerts.reducer';
+import users from './users/users.reducer';
+
 export default combineReducers({
   alerts,
+  users,
 });
--- client/src/redux/users/users.reducer.js
+++ client/src/redux/users/users.reducer.js
@@ -1,19 +1,34 @@
-import { REGISTER_USER, LOGIN_USER } from './user.types';
+import {
+  REGISTER_START,
+  REGISTER_SUCCESS,
+  REGISTER_FAILURE,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from './user.types';
 
 const INITIAL_STATE = {
   users: [],
-  errors: [],
-  currentUser: {},
+  error: null,
+  currentUser: null,
+  loading: true,
 };
 
 export default function (state = INITIAL_STATE, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case REGISTER_USER:
-      return { ...state, users: payload };
-    case LOGIN_USER:
-      return { ...state, currentUser: payload };
+    case LOGIN_START:
+    case REGISTER_START:
+      return { ...state, loading: true };
+    case REGISTER_SUCCESS:
+      return { ...state, users: payload, loading: false };
+    case LOGIN_SUCCESS:
+      return { ...state, currentUser: payload, loading: false };
+    case REGISTER_FAILURE:
+    case LOGIN_FAILURE:
+      return { ...state, loading: true };
+
     default:
       return state;
   }
