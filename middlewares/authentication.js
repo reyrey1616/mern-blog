0f9806ea783b600d1875850c82f622368e01827d
yobbad16 <dabboyrey@gmail.com>
2020-07-06 02:16:54 +0800

    Added environment variables

---
+++ middlewares/authentication.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./async');
+const Users = require('../models/User');
+
+// Protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // If no cookie. the auth middleware will find the token in authorization header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  // If using cookies. if the token is stored in cookies then will assign it to token and use every request
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  //   Make sure token exists
+  if (!token) {
+    return next(res.status(401).json({ error: 'No token' }));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (err) {
+    console.error(err);
+  }
+});
