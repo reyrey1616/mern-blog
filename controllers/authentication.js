bcf5e03c306c86b8b6027ba0f61a652782d86f3d
yobbad16 <dabboyrey@gmail.com>
2020-07-06 01:49:41 +0800

    Created controller for login authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -2,6 +2,36 @@ const Users = require('../models/User');
 const asyncHandler = require('../middlewares/asyncHandler');
 const crypto = require('crypto');
 
+//@desc  Login user
+//@route POST /api/auth/login
+//@access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ error: 'Please provide an email and password' });
+  }
+
+  //   Check for user
+  const user = await Users.findOne({ email }).select('+password');
+  if (!user) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+
+  //   Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+
+  sendTokenResponse(user, 200, res);
+});
+
+// Utility function to send token response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
