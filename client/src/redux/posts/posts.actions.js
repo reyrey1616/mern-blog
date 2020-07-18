bff4e5f7eb2819e92da88f3ffded45e4e59f5c48
yobbad16 <dabboyrey@gmail.com>
2020-07-19 02:24:37 +0800

    Created actions for posts

---
+++ client/src/redux/posts/posts.actions.js
@@ -0,0 +1,16 @@
+import { ADD_POST_START, ADD_POST_SUCCESS, ADD_POST_FAIL } from './posts.types';
+
+export const addPostStart = (payload) => ({
+  type: ADD_POST_START,
+  payload,
+});
+
+export const addPostSuccess = (payload) => ({
+  type: ADD_POST_SUCCESS,
+  payload,
+});
+
+export const addPostFail = (error) => ({
+  type: ADD_POST_START,
+  payload: error,
+});
