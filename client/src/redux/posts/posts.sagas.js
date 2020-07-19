63b5757f6e2a979ee956934b50865fb449ce3f61
yobbad16 <dabboyrey@gmail.com>
2020-07-20 02:39:57 +0800

    Created Saga for getting post

--- client/src/redux/posts/posts.sagas.js
+++ client/src/redux/posts/posts.sagas.js
@@ -1,6 +1,11 @@
 import { takeLatest, put, call, all } from 'redux-saga/effects';
-import { ADD_POST_START } from './posts.types';
-import { addPostSuccess, addPostFail } from './posts.actions';
+import { ADD_POST_START, GET_POST_START } from './posts.types';
+import {
+  addPostSuccess,
+  addPostFail,
+  getPostsSuccess,
+  getPostsFail,
+} from './posts.actions';
 import axios from 'axios';
 
 export function* addPost({ payload: content }) {
@@ -23,9 +28,23 @@ export function* addPost({ payload: content }) {
     yield put(addPostFail(error));
   }
 }
+
+export function* getPosts() {
+  try {
+    const res = yield axios.get('http://localhost:5000/api/blogs');
+    console.log(res);
+    yield put(getPostsSuccess(res.data));
+  } catch (error) {
+    yield put(getPostsFail(error));
+  }
+}
 export function* onAddPost() {
   yield takeLatest(ADD_POST_START, addPost);
 }
+
+export function* onGetPosts() {
+  yield takeLatest(GET_POST_START, getPosts);
+}
 export function* postsSagas() {
-  yield all([call(onAddPost)]);
+  yield all([call(onAddPost), call(onGetPosts)]);
 }
